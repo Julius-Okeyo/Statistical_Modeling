844c6bbd7725af58e90ffd061fa1a001c3cefda7
Julius-Okeyo <jaykopiyo@gmail.com>
2022-08-29 13:58:49 +0300

    Updated covid spread

--- Covid_Spread.py
+++ Covid_Spread.py
@@ -17,47 +17,56 @@ P_N - Probability that a person contacts covid-19 from interacting
       with an infected person.
 
 '''
+from plumbum import cli
+from plumbum import cmd
 
-global Total_Population
-global Initial_Number_of_Infections
-global P_I
-global P_SS
-global P_N
-global Number_of_New_Infection
-global Superspreader_Interactions
-global Non_Superspreader_Interactions
-
-Number_of_New_Infections = 0
-Total_Population = int(input('Total population: '))
-P_I =  float(input('Infection rate: '))
-Initial_Number_of_Infections = P_I * Total_Population
-P_SS = float(input('Probability of a randomly selected person being a superspreader: '))
-Superspreader_Interactions = int(input('Average number of people a superspreader interacts with per day: '))
-Non_Superspreader_Interactions = int(input('Average number of people a non-superspreader interacts with per day: '))
-P_N = float(input('Probability of a new infection resulting from an interaction: '))
-
-def Probability_of_New_Infection(P_I,P_SS,P_N):
-    global Number_of_New_Infections
-    #Probability that a person is a superspreader and is infected with COVID-19.
-    P_SS_I = P_SS * P_I
-    #Probability that a superspreader interacts with a uninfected person.
-    P_SI_I_n = (1 - P_I)
-    #Probability a new infection results from the interaction
-    P1 = P_SS_I * P_SI_I_n * P_N
-     #Probability that a person is a non-superspreader and is infected with COVID-19.
-    P_NS_I = (1 - P_SS) * P_I
-    #Probability that a non-superspreader interacts with a uninfected person.
-    P_NI_I = (1 - P_I)
-    #Probability a new infection results from the interaction
-    P2 = P_NS_I * P_NI_I * P_N
-    Number_of_New_Infections = round((P1 * Superspreader_Interactions) + (P2 * Non_Superspreader_Interactions))
-
-def Time_Series_Projection(Number_of_Days):
+class IR_Projection(cli.Application):
+    global Total_Population
+    global Initial_Number_of_Infections
     global P_I
-    Total_Infection = Initial_Number_of_Infections
-    for days in range (1,Number_of_Days+1):
-        Probability_of_New_Infection(P_I,P_SS,P_N)
-        Total_Infection += Number_of_New_Infections
-        P_I = Total_Infection / Total_Population
-    print('Total number of infected people: ',Total_Infection)
-    return Total_Infection
+    global P_SS
+    global P_N
+    global Number_of_New_Infection
+    global Superspreader_Interactions
+    global Non_Superspreader_Interactions
+
+    Number_of_New_Infections = 0
+    Total_Population = int(input('Total population: '))
+    P_I =  float(input('Infection rate: '))
+    Initial_Number_of_Infections = P_I * Total_Population
+    P_SS = float(input('Probability of a randomly selected person being a superspreader: '))
+    Superspreader_Interactions = int(input('Average number of people a superspreader interacts with per day: '))
+    Non_Superspreader_Interactions = int(input('Average number of people a non-superspreader interacts with per day: '))
+    P_N = float(input('Probability of a new infection resulting from an interaction: '))
+
+    def Probability_of_New_Infection(self,P_I,P_SS,P_N):
+        global Number_of_New_Infections
+        #Probability that a person is a superspreader and is infected with COVID-19.
+        P_SS_I = P_SS * P_I
+        #Probability that a superspreader interacts with a uninfected person.
+        P_SI_I_n = (1 - P_I)
+        #Probability a new infection results from the interaction
+        P1 = P_SS_I * P_SI_I_n * P_N
+        #Probability that a person is a non-superspreader and is infected with COVID-19.
+        P_NS_I = (1 - P_SS) * P_I
+        #Probability that a non-superspreader interacts with a uninfected person.
+        P_NI_I = (1 - P_I)
+        #Probability a new infection results from the interaction
+        P2 = P_NS_I * P_NI_I * P_N
+        Number_of_New_Infections = round((P1 * Superspreader_Interactions) + (P2 * Non_Superspreader_Interactions))
+
+    def Time_Series_Projection(self,Number_of_Days):
+        global P_I
+        Total_Infection = Initial_Number_of_Infections
+        for days in range (1,Number_of_Days+1):
+            self.Probability_of_New_Infection(P_I,P_SS,P_N)
+            Total_Infection += Number_of_New_Infections
+            P_I = Total_Infection / Total_Population
+        print('Total number of infected people: ',Total_Infection)
+
+    def main(self):
+        self.Time_Series_Projection(int(input('Projection period in days: ')))
+        cmd.ls(__path__)
+
+if __name__ == '__main__':
+    IR_Projection.run()
